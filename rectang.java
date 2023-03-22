802b27bd27c1d14d575380ff30286e05f7245499
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-03-22 16:48:27 +0700

    hoan thanh

---
+++ rectang.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class rectang {
+    public static void main(String[] args) {
+        System.out.println("input width of rectang");
+        Scanner width= new Scanner(System.in);
+        int width1=width.nextInt();
+        System.out.println("input width of rectang");
+        Scanner height = new Scanner(System.in);
+        int height1= height.nextInt();
+        System.out.println("the area of rectang is: " + (width1*height1));
+
+    }
+}
